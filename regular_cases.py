60d62f2f0f1864d53f2305e8138d55642e83c846
alfonta-datetime <shahar.alfonta@gmail.com>
2023-08-22 01:46:10 +0300

    finished algorithm 1

--- regular_cases.py
+++ regular_cases.py
@@ -31,10 +31,18 @@ def algorithm_1(n: DeltaNumber):
         ci = (xi + yi + zi + n.carry[i - 1] - n[i - 1]) // g
         n.p1[i], n.p2[i], n.p3[i], n.carry[i] = xi, yi, zi, ci
 
-    # step m+1
-    n.p1[m+1] = 0
+    # adjustment step
+    cm = ci  # NOQA
+    if cm == 1:
+        n.p1[m + 1] = 0
+    elif cm == 0:
+        n.p1[m + 1] = 1
+    elif cm == 2:
+        n.p1[m + 1] = 0
+        n.p2[m + 1] -= 1
+        n.p2[m] -= 1
+        n.p3[m] = 0
 
-    # TODO: The adjustment step
     return n
 
 
@@ -62,22 +70,22 @@ def regular_cases(n: DeltaNumber):
             return algorithm_2(n)
         elif n.ntype in (NType.A_5, NType.A_6):
             return algorithm_1(n)
-        else:
+        else:  # B types
             return algorithm_4(n)
     else:
         if n.ntype in (NType.A_1, NType.A_2, NType.A_3, NType.A_4):
             return algorithm_1(n)
         elif n.ntype in (NType.A_5, NType.A_6):
             return algorithm_2(n)
-        else:
+        else:  # B types
             return algorithm_3(n)
 
 
 if __name__ == '__main__':
-    n = DeltaNumber(135_948_762, 10)
+    n = DeltaNumber(314159265358979323846, 10)
     regular_cases(n)
     print(n)
     print(n.p1)
     print(n.p2)
     print(n.p3)
-    assert n.p1 + n.p2 + n.p3 == n
+    print(f"{n.p1} + {n.p2} + {n.p3} == {n.p1 + n.p2 + n.p3}")
