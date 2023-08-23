77b00a6e9a06983580298d3d271d73868c31510c
alfonta-datetime <shahar.alfonta@gmail.com>
2023-08-23 20:50:05 +0300

    changed algorithms to methods, added algorithm 4 adjustment step

--- regular_cases.py
+++ regular_cases.py
@@ -7,25 +7,9 @@ def algorithm_1(n: DeltaNumber):
     x, y, z = n.p1, n.p2, n.p3
     c = n.c
 
-    # step 1 - define the carry, done in the __init__ of the DeltaNumber object
-    # step 2
-    if z[1] <= n[2 * m - 2] - 1:
-        x[2] = n.D(n[2 * m - 1] - y[1])
-    else:
-        x[2] = n.D(n[2 * m - 1] - y[1] - 1)
-    y[2] = n.D(n[2 * m - 2] - z[1] - 1)
-    z[2] = n.D(n[1] - x[2] - y[2] - c[1])
-    n.carry(2)
-
-    # steps 3<=i<=m
+    n.step_2(0, 0)
     for i in range(3, m + 1):
-        if z[i - 1] <= n[2 * m - i] - 1:
-            x[i] = 1
-        else:
-            x[i] = 0
-        y[i] = n.D(n[2 * m - i] - z[i - 1] - 1)
-        z[i] = n.D(n[i - 1] - x[i] - y[i] - c[i - 1])
-        n.carry(i)
+        n.step_i(i, 0, 0)
 
     # adjustment step
     if c[m] == 1:
@@ -47,25 +31,12 @@ def algorithm_2(n: DeltaNumber):
     x, y, z = n.p1, n.p2, n.p3
     c = n.c
 
-    # step 1 - define the carry, done in the __init__ of the DeltaNumber object
     # step 2
-    if z[1] <= n[2 * m - 3] - 1:
-        x[2] = n.D(n[2 * m - 2] - y[1])
-    else:
-        x[2] = n.D(n[2 * m - 2] - y[1] - 1)
-    y[2] = n.D(n[2 * m - 3] - z[1] - 1)
-    z[2] = n.D(n[1] - x[2] - y[2] - c[1])
-    n.carry(2)
+    n.step_2(1, 0)
 
     # steps 3<=i<=m-1
     for i in range(3, m):
-        if z[i - 1] <= n[2 * m - i - 1] - 1:
-            x[i] = 1
-        else:
-            x[i] = 0
-        y[i] = n.D(n[2 * m - i - 1] - z[i - 1] - 1)
-        z[i] = n.D(n[i - 1] - x[i] - y[i] - c[i - 1])
-        n.carry(i)
+        n.step_i(i, 1, 0)
 
     # step m
     x[m] = 0
@@ -104,25 +75,9 @@ def algorithm_3(n: DeltaNumber):
     x, y, z = n.p1, n.p2, n.p3
     c = n.c
 
-    # step 1 - define the carry, done in the __init__ of the DeltaNumber object
-    # step 2
-    if z[1] <= n[2 * m - 3] - 1:
-        x[3] = n.D(n[2 * m - 2] - y[1])
-    else:
-        x[3] = n.D(n[2 * m - 2] - y[1] - 1)
-    y[2] = n.D(n[2 * m - 3] - z[1] - 1)
-    z[2] = n.D(n[1] - x[2] - y[2] - c[1])
-    n.carry(2)
-
-    # steps 3<=i<=m-1
+    n.step_2(1, 1)
     for i in range(3, m):
-        if z[i - 1] <= n[2 * m - i - 1] - 1:
-            x[i + 1] = 1
-        else:
-            x[i + 1] = 0
-        y[i] = n.D(n[2 * m - i - 1] - z[i - 1] - 1)
-        z[i] = n.D(n[i - 1] - x[i] - y[i] - c[i - 1])
-        n.carry(i)
+        n.step_i(i, 1, 1)
 
     # step m
     x[m + 1] = 0
@@ -162,30 +117,181 @@ def algorithm_4(n: DeltaNumber):
     x, y, z = n.p1, n.p2, n.p3
     c = n.c
 
-    # step 1 - define the carry, done in the __init__ of the DeltaNumber object
-    # step 2
-    if z[1] <= n[2 * m - 4] - 1:
-        x[3] = n.D(n[2 * m - 3] - y[1])
-    else:
-        x[3] = n.D(n[2 * m - 3] - y[1] - 1)
-    y[2] = n.D(n[2 * m - 4] - z[1] - 1)
-    z[2] = n.D(n[1] - x[2] - y[2] - c[1])
-    n.carry(2)
-
-    # steps 3<=i<=m-2
+    n.step_2(2, 1)
     for i in range(3, m - 1):
-        if z[i - 1] <= n[2 * m - i - 2] - 1:
-            x[i + 1] = 1
+        n.step_i(i, 2, 1)
+
+    # step m -1
+    if z[m - 2] <= n[m - 1] - 1:
+        x[m] = 1
+    else:
+        x[m] = 0
+    y[m - 1] = n.D(n[m - 1] - z[m - 2] - 1)
+    z[m - 1] = n.D(n[m - 2] - x[m - 1] - y[m - 1] - c[m - 2])
+
+    # adjustment step
+    if x[m] + c[m - 1] == 0 and y[m - 1] != g - 1:
+        if z[m - 1] != 0:
+            y[m] += 1
+            z[m - 1] -= 1
+        elif y[m - 2] != 0:
+            if y[m - 1] != 1:
+                if z[m - 2] != g - 1:
+                    x[m + 1] = 1
+                    y[m + 1] -= 1
+                    y[m] -= 1
+                    z[m] += 1
+                    z[m - 1] = 1
+                else:
+                    x[m + 1] = 2
+                    y[m + 1] -= 1
+                    y[m] -= 2
+                    z[m] = 0
+                    z[m - 1] = 3
+            else:
+                x[m + 1] = 1
+                y[m + 1] -= 1
+                y[m] = g - 1
+                z[m] = 0
+                z[m - 1] = 3
         else:
-            x[i + 1] = 0
-        y[i] = n.D(n[2 * m - i - 2] - z[i - 1] - 1)
-        z[i] = n.D(n[i - 1] - x[i] - y[i] - c[i - 1])
-        n.carry(i)
-
-    # step m
-    x[m + 1] = 0
-    y[m] = n.D(n[m - 1] - z[m - 1] - x[m] - c[m - 1])
-
+            if z[m - 2] != g - 1:
+                x[m + 2] -= 1
+                x[m + 1] = 1
+                y[m + 1] = g - 1
+                y[m] -= 1
+                z[m] += 1
+                z[m - 1] = 1
+            elif y[m - 1] != 1:
+                x[m + 2] -= 1
+                x[m + 1] = 2
+                y[m + 1] = g - 1
+                y[m] -= 2
+                z[m] = 0
+                z[m - 1] = 3
+            else:
+                x[m + 2] -= 1
+                x[m + 1] = 1
+                y[m + 1] = y[m] = g - 1
+                z[m] = 0
+                z[m - 1] = 3
+    elif x[m] + c[m - 1] == 0 and y[m - 1] == g - 1:
+        x[m + 1] = 1
+        y[m + 1] -= 1
+        y[m] = g - 2
+        z[m] += 1
+        z[m - 1] = 0
+    elif c[m - 1] == 2 and x[m] == 0:
+        if z[m - 1] != g - 1:
+            y[m] -= 1
+            z[m - 1] += 1
+        elif z[m - 2] != g - 1:
+            if y[m - 2] != 0:
+                x[m + 1] = 1
+                y[m + 1] -= 1
+                y[m] -= 2
+                z[m] += 1
+                z[m - 1] = 1
+            else:
+                x[m + 2] -= 1
+                x[m + 1] = 1
+                y[m + 1] = g - 1
+                y[m] -= 2
+                z[m] += 1
+                z[m - 1] = 1
+        else:
+            if y[m - 1] not in (g - 1, g - 2):
+                if y[m - 2] != g - 1:
+                    x[m + 2] -= 1
+                    x[m + 1] = g - 2
+                    y[m + 1] += 1
+                    y[m] += 2
+                    z[m] = z[m - 1] = g - 2
+                else:
+                    x[m + 1] = g - 2
+                    y[m + 1] = 0
+                    y[m] += 2
+                    z[m] = z[m - 1] = g - 2
+            else:
+                if y[m - 2] >= 1:
+                    x[m + 1] = 2
+                    y[m + 1] -= 1
+                    y[m] -= 3
+                    z[m] = 0
+                    z[m - 1] = 3
+                else:
+                    x[m+2] -= 1
+                    x[m+1] = 2
+                    y[m+1] = g -1
+                    y[m] -= 3
+                    z[m] = 0
+                    z[m - 1] = 3
+    elif c[m - 1] == 1 and x[m] == 1:
+        if z[m - 1] != g - 1:
+            if y[m - 1] != 0:
+                y[m] -= 1
+                z[m-1] += 1
+            else:
+                x[m+1] = 0
+                y[m] = g- 1
+                z[m-1] += 2
+        elif z[m-2] != 0:
+            if y[m-2] != g- 1:
+                x[m+1] = 0
+                y[m+1] += 1
+                y[m] += 1
+                z[m] -= 1
+                z[m-1] = g - 2
+            elif y[m - 1] not in (0, 1):
+                x[m+1] = 2
+                x[m+1] = g - 2
+                y[m] -= 2
+                z[m] += 1
+                z[m-1] = 1
+            elif y[m - 1] == 0:
+                y[m+1] = y[m] = g - 2
+                z[m] += 1
+                z[m - 1] = 1
+            else:
+                y[m + 1] = g - 2
+                y[m] = g -1
+                z[m] += 1
+                z[m - 1] = 1
+        elif y[m - 2] != 0:
+            if y[m - 1] not in (0, 1):
+                x[m+1] = 2
+                y[m + 1] -= 1
+                y[m] -= 2
+                z[m] = z[m-1] = 1
+            elif y[m-1] == 0:
+                y[m+1] -= 1
+                y[m] = g - 2
+                z[m] = z[m-1] = 1
+            else:
+                y[m + 1] -= 1
+                y[m] = g - 1
+                z[m] = z[m - 1] = 1
+        else:
+            if y[m - 1] not in (0, 1):
+                x[m+2] -= 1
+                x[m+1] = 2
+                y[m+1] = g - 1
+                y[m] -= 2
+                z[m] = z[m - 1] = 1
+            elif y[m-1] == 0:
+                x[m + 2] -= 1
+                x[m + 1] = 1
+                y[m + 1] = g - 1
+                y[m] = g -2
+                z[m] = z[m - 1] = 1
+            else:
+                x[m + 2] -= 1
+                x[m + 1] = 1
+                y[m + 1] = y[m] = g - 1
+                z[m] = z[m - 1] = 1
+    elif x[m] + c[m - 1] == 3:
+        y[m] -= 1
+        z[m-1] = 0
 
 def algorithm_5(n: DeltaNumber):
     pass
@@ -211,7 +317,9 @@ def regular_cases(n: DeltaNumber):
 
 
 if __name__ == '__main__':
-    n = DeltaNumber(120205690315959428539, 10)
+    # n = DeltaNumber(314159265358979323846, 10)  # algo 1
+    # n = DeltaNumber(2718281828459045235360, 10)  # algo 2
+    n = DeltaNumber(120205690315959428539, 10)  # algo 3
     regular_cases(n)
     print(n)
     print(n.p1)
